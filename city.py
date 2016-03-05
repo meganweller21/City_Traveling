9a1426d6a5e3df8240ac91787244a06f73049d15
meganweller21 <mweller213@gmail.com>
2016-03-04 21:38:10 -0700

    Updated task 3, does't work yet

--- city.py
+++ city.py
@@ -6,7 +6,7 @@ import re
 def read_file(file_name):
 	file = open(file_name, "r")
 	line = file.readline().replace("-----------------------", "").replace("\t", "")    
-	map = []
+	results = []
 	
 	while line:
 		if (line == '\n'):  #we don't want to read in a blank line
@@ -15,14 +15,15 @@ def read_file(file_name):
 			line = line.strip() #strip unnessesary white space
 			line = re.sub('\s\s+', ',', line)   #replace lone tabs with a comma
 			fromCity, toCity, miles = line.split(',')  # splits line into list of tokens
-			map.insert(0, (fromCity, toCity, miles))   # insert into the map
+		
+			results.insert(0, (fromCity, toCity, miles))   # insert into the map
 			
 			line = file.readline().replace("\t", " ").replace("-----------------------", "")
 	   
 		
-	map.reverse()                # original order
+	results.reverse()                # original order
 	#print(map)
-	return map
+	return results
 
 def open_map(file_name):
 	cityMap = read_file(file_name)
@@ -37,7 +38,7 @@ def open_map(file_name):
 
 
 
-def solve_map(map):
+def solve_map(results):
 	print ("Welcome to the new Montana Road Network")
 	print ("1. Enter 1 to see how many cities are connected to a specific city")
 	print ("2. Enter 2 to see if two cities have a direct connection")
@@ -50,20 +51,20 @@ def solve_map(map):
 
 	if (userIn == '1'):
 		cityIn = input("Enter the city: ")
-		print(find_city_num(map, cityIn))
+		print(find_city_num(results, cityIn))
 	elif (userIn == '2'):
 		city1In = input("First City: ")
 		city2In = input("Second City:")
-		find_edge(map, city1In, city2In)
+		print(find_edge(results, city1In, city2In))
 	elif (userIn == '3'):
 		city1In = input("First City: ")
 		city2In = input("Second City: ")
 		hopsIn = input("Hop connection: ")
-		hop_connection(map, city1In, city2In, hopsIn)
+		print(hop_connection(results, city1In, city2In, hopsIn))
 	elif (userIn == '4'):
 		city1In = input("First City: ")
 		city2In = input("Second City: ")
-		print(city_connection(map, city1In, city2In))
+		print(city_connection(results, city1In, city2In))
 	else:
 		print("quitting")
 
@@ -72,8 +73,8 @@ def solve_map(map):
 #Complete!
 def find_city_num(maps, city):
 	count = 0
-	for map in maps:
-		if map[0] == city or map[1] == city:
+	for result in results:
+		if result[0] == city or result[1] == city:
 			count+=1
 
 	return count
@@ -83,9 +84,9 @@ def find_city_num(maps, city):
 def find_edge(maps, city1, city2):
 	miles = 0
 
-	for map in maps:
-		if (map[0] == city1 and map[1] == city2):
-			miles = map[2]
+	for result in results:
+		if (result[0] == city1 and result[1] == city2):
+			miles = result[2]
 	
 	if(miles != 0):
 		return miles
@@ -94,16 +95,36 @@ def find_edge(maps, city1, city2):
 
 #Task 3. Given two query cities and an integer d, return YES/NO for whether there is a k-hop connection,
 #k ≤ d, between them; if YES, print one solution out, to gether with the total distance of the d hops.
-def hop_connection(maps, city1, city2, d):
-	print("hi")
 
+def hop_connection(results, city1, city2, d):
+	miles = 0
+	mapCity = city1
+	i = 0
+	netMiles = 0
+	
+	mapConnections = []
+
+	for result in results:
+		if(result[0] == mapCity or i <= int(d)):
+			#print(mapCity)
+			mapCity = result[1]
+
+			netMiles = result[2]
+			#print(map[1])
+			i+1
+			
+		if result[1] == city2:
+			mapConnections.insert(0, (netMiles))
+			break;
+			
+	
+	mapConnections.reverse()
+	return mapConnections[0]
+	
 #Task 4. Given two query cities, return YES/NO for whether there is a connection (not necessarily direct)
 #between them; if YES, print one solution out, together with the actual total distance of the connection.
-def city_connection(maps, city1, city2):
-	
-
-
-
+def city_connection(results, city1, city2):
+	print("hi")
 
 
 open_map("citymap.txt")
